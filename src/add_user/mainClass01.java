590ee3235f7d4d27c80a313568910f69e6bff983
문성민 <mkmm1104@naver.com>
2023-07-07 14:03:23 +0900

    2번 수정완료

--- src/add_user/mainClass01.java
+++ src/add_user/mainClass01.java
@@ -1,13 +1,14 @@
 package add_user;
 
 import java.util.Scanner;
+import sign.*;
 
 public class mainClass01 {
 public static void main(String[] args) {
 	Scanner input = new Scanner(System.in);
 	signUp sign = new signUpImpl();
-	int num;
-	
+	Sign s = new SignImp();
+	int num, result =0;
 	while(true) {
 		System.out.println("1. 회원가입");
 		System.out.println("2. 로그인");
@@ -16,10 +17,16 @@ public static void main(String[] args) {
 		System.out.print(">>>");
 		num = input.nextInt();
 		switch (num) {
+
 		case 1:
 			sign.display();
 			break;
-		case 2: break;
+		case 2: 
+			if(result == 1) {
+				System.out.println("이미 로그인 중입니다.");
+			}else {
+				result = s.signIn();
+			}break;
 		case 3: break;
 		case 4: break;
 		}
